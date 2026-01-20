d6cb91af8db7ea4a675f45dcb7ef68d8ff8c3d08
royhsle0120 <royhslee0120@gmail.com>
2026-01-20 18:18:35 +0900

    알고리즘 코드카타 1번 문제 완료

---
+++ src/algorithmCode/test1/Solution1.java
@@ -0,0 +1,11 @@
+package algorithmCode.test1;
+
+public class Solution1 {
+
+    public int solution1(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+
+}
