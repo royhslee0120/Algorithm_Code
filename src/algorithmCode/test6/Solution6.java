ac7c6473882c493267506c8eb64b323778df9469
royhsle0120 <royhslee0120@gmail.com>
2026-01-22 20:37:57 +0900

    알고리즘 코드카타 6번 문제 완료

---
+++ src/algorithmCode/test6/Solution6.java
@@ -0,0 +1,11 @@
+package algorithmCode.test6;
+
+public class Solution6 {
+
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 + num2;
+        return answer;
+    }
+
+}
