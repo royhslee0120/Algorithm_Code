dd384b18cd455f476786c77aad758eba06dd3754
royhsle0120 <royhslee0120@gmail.com>
2026-01-23 19:15:18 +0900

    알고리즘 코드카타 7번 문제 완료

---
+++ src/algorithmCode/test7/Solution7.java
@@ -0,0 +1,10 @@
+package algorithmCode.test7;
+
+public class Solution7 {
+
+    public int solution(int num1, int num2) {
+        double result = (double) num1 / num2 * 1000;
+        return (int) result;
+    }
+
+}
