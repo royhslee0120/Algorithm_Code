e04d7d2c12eb735100d9554acc13d6430bc34be9
royhsle0120 <royhslee0120@gmail.com>
2026-01-29 09:29:03 +0900

    알고리즘 코드카타 13번 문제 완료

---
+++ src/algorithmCode/test13/Solution13.java
@@ -0,0 +1,12 @@
+package algorithmCode.test13;
+
+public class Solution13 {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
