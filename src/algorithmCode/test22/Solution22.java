2015a26536b1bcfe58a6be1bf799f4713d43331c
royhsle0120 <royhslee0120@gmail.com>
2026-02-25 09:18:49 +0900

    알고리즘 코드카타 22번 문제 완료

---
+++ src/algorithmCode/test22/Solution22.java
@@ -0,0 +1,16 @@
+package algorithmCode.test22;
+
+public class Solution22 {
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        for (int i = start; i <= end; i++) {
+            answer += i;
+        }
+
+        return answer;
+    }
+}
